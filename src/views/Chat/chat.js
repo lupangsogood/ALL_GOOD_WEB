5e177625135c5160a76949337d4c24ff824d8dfe
Anusit Poyen <lupangsogood@gmail.com>
2020-03-08 18:27:36 +0700

    can implement layout

--- src/views/Chat/chat.js
+++ src/views/Chat/chat.js
@@ -1,12 +1,26 @@
 import React, { Component, Suspense } from "react"
-import MessengerCustomerChat from "react-messenger-customer-chat"
+import { Container, Row, Col } from "reactstrap"
+
+import MessageList from "./MessageList"
+import ConersationList from "./ConversationList"
+
+import "./Messenger/Messenger.css"
 
 class ChatList extends Component {
   render() {
     return (
-      <div>
-        <MessengerCustomerChat pageId="<YOUR_PAGE_ID>" appId="<YOUR_APP_ID>" />
-      </div>
+      <Row className="row justify-content-md-center messenger">
+        <Col xs="12" md="5" xl="3">
+          <Container className="scrollable">
+            <ConersationList />
+          </Container>
+        </Col>
+        <Col xs="3" md="7" xl="7">
+          <Container className="scrollable">
+            <MessageList />
+          </Container>
+        </Col>
+      </Row>
     )
   }
 }
